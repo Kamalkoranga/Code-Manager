e9e63f48d5bf637898c3b5b5c1cdc80b0beeeba8
Kamal Koranga <kamalkornga75@gmail.com>
2022-06-10 19:27:40 +0530

    Coded add func

--- code_manager.py
+++ code_manager.py
@@ -4,33 +4,58 @@
 
 
 from inspect import getsource
-from GTN import gtn
-from calculator import calculate
-from . import *
+from packages.GTN import gtn
+from packages.calculator import calculate
+import shutil
+from packages import *
 
 
 class Code():
-    def __init__(self, language, i=0):
+    def __init__(self, language, i=0, fn='', code=''):
         self.language = language
         self.i = i
+        self.fn = fn
+        self.code = code
 
-        # while self.i == 0:
-        #     self.main()
+        while self.i == 0:
+            self.main()
 
     def main(self):
         print(f">>>>>>>>> CODE MANAGER({self.language}) <<<<<<<<<<")
         print("Features:-")
-        print(" 1 - Show Codes")
+        print(" 1 - Add Code")
         print(" 2 - Run Codes")
-        print(" 3 - Exit")
+        print(" 3 - Show Codes")
+        print(" 4 - Exit")
 
         choose = int(input("Choose any feature: "))
         if choose == 1:
-            self.show()
+            self.add()
         elif choose == 2:
             self.run()
         elif choose == 3:
+            self.show()
+        elif choose == 4:
             self.i = 1
+
+    def add(self):
+        self.fn = input("Enter file name: ")
+        file = open(self.fn, 'x', )
+        # =========== Multi-line input here!!
+        lines = []
+        while True:
+            line = input()
+            if line:
+                lines.append(line +'\n')
+            else:
+                break
+            self.code = '\n'.join(lines)
+        file.writelines(lines) # adds lines variable in file
+        file = open(self.fn) # open the file
+        # print(self.code) # prints the code
+        file.close()
+
+        shutil.move(f"./{self.fn}", f"./packages/{self.fn}")
         
     def run(self):
         which = int(input("Which code you want to run: "))
@@ -41,6 +66,14 @@ class Code():
         elif which == 2:
             print(" ======== Gues The Number =============")
             gtn()
+        
+        elif which == 3:
+            print(f" ======== {self.fn} =============")
+            exec(open(f'packages/{self.fn}').read()) # executes another py file
+
+        elif which == 4:
+            print(f" ======== {self.fn} =============")
+            exec(open(f'packages/{self.fn}').read())
 
     def show(self):
         which = int(input("which: "))
@@ -53,25 +86,14 @@ class Code():
             print(getsource(gtn))
         
         elif which == 3:
-            print(getsource())
+            # print(f" ======== {self.fn.strip('.py')} =============") # strip function allow us to remove character
+            print(f" ======== {self.fn} =============")
+            print(self.code)
 
-    def add(self):
-        fn = input("Enter file name: ")
-        file = open(fn, 'x')
-        # =========== Multi-line input here!!
-        lines = []
-        while True:
-            line = input()
-            if line:
-                lines.append(line)
-            else:
-                break
-        code = '\n'.join(lines) # add all inputs in a list
-
-        file.writelines(lines) # adds lines variable in file
-        file = open(fn) # open the file
-        print(code) # prints the code
+        elif which == 3:
+            print(f" ======== {self.fn} =============")
+            print(self.code)
 
 
-fir = Code('Python')
-fir.add()
+fir = Code('py')
+# fir.add()
